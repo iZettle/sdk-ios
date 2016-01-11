385147a7682961022a860fa87aedd530504a5567
Jens Utbult <jens@izettle.com>
2016-01-11 15:36:15 +0100

    iZettle SDK 1.2.0

--- iZettleSDK/iZettleSDK.framework/Versions/A/Headers/iZettleSDK.h
+++ iZettleSDK/iZettleSDK.framework/Versions/A/Headers/iZettleSDK.h
@@ -5,9 +5,11 @@
 
 #import <UIKit/UIKit.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @class iZettleSDKPaymentInfo;
 
-typedef void(^iZettleSDKOperationCompletion)(iZettleSDKPaymentInfo *paymentInfo, NSError *error);
+typedef void(^iZettleSDKOperationCompletion)(iZettleSDKPaymentInfo * _Nullable paymentInfo, NSError * _Nullable error);
 
 @interface iZettleSDK : NSObject
 
@@ -17,7 +19,7 @@ typedef void(^iZettleSDKOperationCompletion)(iZettleSDKPaymentInfo *paymentInfo,
 - (void)startWithAPIKey:(NSString *)apiKey;
 
 /*! if set only an iZettle user with this account will be allowed to be logged in. */
-@property (nonatomic, copy) NSString *enforcedUserAccount;
+@property (nonatomic, copy, nullable) NSString *enforcedUserAccount;
 
 @end
 
@@ -25,19 +27,20 @@ typedef void(^iZettleSDKOperationCompletion)(iZettleSDKPaymentInfo *paymentInfo,
 
 /*! Perform a payment with an amount and a reference. */
 - (void)chargeAmount:(NSDecimalNumber *)amount
-            currency:(NSString *)currency
-           reference:(NSString *)reference
+            currency:(nullable NSString *)currency
+           reference:(nullable NSString *)reference
 presentFromViewController:(UIViewController *)viewController
           completion:(iZettleSDKOperationCompletion)completion;
 
 /*! Refund a payment with a given reference. */
 - (void)refundPaymentWithReference:(NSString *)reference
-                   refundReference:(NSString *)refundReference
+                   refundReference:(nullable NSString *)refundReference
          presentFromViewController:(UIViewController *)viewController
                         completion:(iZettleSDKOperationCompletion)completion;
 
 /*! Query iZettle for payment information of a payment with a given reference. */
 - (void)retrievePaymentInfoForReference:(NSString *)reference
+              presentFromViewController:(UIViewController *)viewController
                              completion:(iZettleSDKOperationCompletion)completion;
 
 /*! Present iZettle settings view. The user can switch account, access the iZettle FAQ, view card reader settings etc. */
@@ -53,7 +56,7 @@ presentFromViewController:(UIViewController *)viewController
 
 @interface iZettleSDKPaymentInfo : NSObject
 
-@property (nonatomic, readonly) NSDictionary *dictionary;   // Dictionary representation of the payment information
+@property (nonatomic, readonly) NSDictionary<NSString *, id> *dictionary;   // Dictionary representation of the payment information
 
 @property (nonatomic, readonly) NSString *referenceNumber;  // iZettles reference to the payment
 @property (nonatomic, readonly) NSString *entryMode;        // EMV, MAGSTRIPE, MANUAL_ENTRY
@@ -63,13 +66,15 @@ presentFromViewController:(UIViewController *)viewController
 @property (nonatomic, readonly) NSString *panHash;          // Hash sum of the plain pan
 @property (nonatomic, readonly) NSString *cardBrand;
 
-@property (nonatomic, readonly) NSString *AID;
-@property (nonatomic, readonly) NSString *TSI;
-@property (nonatomic, readonly) NSString *TVR;
-@property (nonatomic, readonly) NSString *applicationName;
+@property (nonatomic, readonly, nullable) NSString *AID;
+@property (nonatomic, readonly, nullable) NSString *TSI;
+@property (nonatomic, readonly, nullable) NSString *TVR;
+@property (nonatomic, readonly, nullable) NSString *applicationName;
 
 // Only used for certain markets
-@property (nonatomic, readonly) NSNumber *numberOfInstallments;
-@property (nonatomic, readonly) NSDecimalNumber *installmentAmount;
+@property (nonatomic, readonly, nullable) NSNumber *numberOfInstallments;
+@property (nonatomic, readonly, nullable) NSDecimalNumber *installmentAmount;
 
 @end
+
+NS_ASSUME_NONNULL_END
