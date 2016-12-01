bed6f2d703be18f05b78f53ed819948e2ab674da
Jens Utbult <jens@izettle.com>
2016-12-01 14:09:07 +0100

    Updated SDK to 1.3.1

---
+++ Sample/iZettleSDKSample/ConsoleViewController.h
@@ -0,0 +1,14 @@
+//
+//  ConsoleViewController.h
+//  iZettle SDK Sample
+//
+//  Copyright (c) 2015 iZettle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConsoleViewController : UIViewController
+
+- (void)addToLog:(id)object;
+
+@end
