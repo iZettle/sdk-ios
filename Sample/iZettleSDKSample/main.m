2b8eb583676c11a5eb02a7fb28a4180bdb123222
Alexander Cyon <alexander.cyon@izettle.com>
2016-12-20 10:19:34 +0100

    Updated SDK to 1.3.1

---
+++ Sample/iZettleSDKSample/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  iZettle SDK Sample
+//
+//  Copyright (c) 2015 iZettle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
