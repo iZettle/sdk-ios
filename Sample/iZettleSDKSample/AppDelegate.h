323cd53e81ff4182d158037130dcdac02024f159
Adeel Akhter <adeelakhter@Adeels-MacBook-Pro-2.local>
2017-01-17 15:53:18 +0100

    Updated SDK to 1.3.1

---
+++ Sample/iZettleSDKSample/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  iZettle SDK Sample
+//
+//  Copyright (c) 2015 iZettle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
