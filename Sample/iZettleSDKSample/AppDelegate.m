3a24989a2b9f4faae4acfde8c4770d8b9dbe1819
Alexander Cyon <alexander.cyon@izettle.com>
2017-01-23 16:19:10 +0100

    Updated SDK to 1.4.0

---
+++ Sample/iZettleSDKSample/AppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.m
+//  iZettle SDK Sample
+//
+//  Copyright (c) 2015 iZettle. All rights reserved.
+//
+
+#import "AppDelegate.h"
+@import iZettleSDK;
+#import <iZettleSDK/iZettleSDK.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [[iZettleSDK shared] startWithAPIKey:@"demo-app-izorn"];
+    return YES;
+}
+
+@end
