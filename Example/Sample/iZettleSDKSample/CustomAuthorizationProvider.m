f2ab24b9ae94b2b6a50393770b3fe997ceb977bb
iOS <apptest@izettle.com>
2021-03-26 13:34:42 +0000

    Updated SDK to v3.1.0

--- Example/Sample/iZettleSDKSample/CustomAuthorizationProvider.m
+++ Example/Sample/iZettleSDKSample/CustomAuthorizationProvider.m
@@ -2,7 +2,7 @@
 //  CustomAuthorizationProvider.m
 //  iZettle SDK Sample
 //
-//  Copyright (c) 2015 iZettle. All rights reserved.
+//  Copyright © 2015 PayPal Inc. All rights reserved.
 //
 
 #import "CustomAuthorizationProvider.h"
