30b5ef96cbc89ab245ce3069fe4eb4e36dca76ad
Paul Peelen <ppeelen@paypal.com>
2023-08-29 10:48:54 +0200

    Reverted use of temp token (#438)

--- Example/iZettleSDKSample/AppDelegate.m
+++ Example/iZettleSDKSample/AppDelegate.m
@@ -16,8 +16,8 @@
 /// for an example of how to do that - see `CustomAuthorizationProvider.h`,
 /// and to try it out set `useCustomAuthorizationProvider` to `YES`
 static BOOL const useCustomAuthorizationProvider = NO;
-static NSString * const clientId = @"1a1f43cf-73eb-4fa2-a57d-db29b7cf9c88";
-static NSString * const callbackURL = @"integration-tester://login.callback";
+static NSString * const clientId = @"<client id from developer portal>";
+static NSString * const callbackURL = @"izettle-iZorn://login.callback";
 
 @implementation AppDelegate
 
