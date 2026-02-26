cb23fe611b4d8961c8c43a7a3a7f7e93de2fac5d
Anka <runner@iad20-fj925-0e91740c-bb6d-485d-8bed-6b557f7a968a-FEB2D69844EB.local>
2026-02-26 14:16:34 +0000

    Updated SDK to v4.70.3

--- Package.swift
+++ Package.swift
@@ -1,6 +1,13 @@
 // swift-tools-version: 5.7
 import PackageDescription
 
+let version = "4.70.3"
+let frameworks = [
+    "iZettleSDK": "84a1d5aed9c333db716458e5900744eabdb99f63cc0ee44056acfe5d206e638a",
+    "iZettlePayments": "5c4c7856e8d7093e43412db09e788c4194e5d5f0b4ae095788407250662a10b4",
+    "PPRiskMagnes": "7734737a9c399fc93725099ab7e946cc32add965bbd69ab9d432bff4da7f8d0a"
+]
+
 let package = Package(
     name: "sdk-ios",
     platforms: [
@@ -9,18 +16,11 @@ let package = Package(
     products: [
         .library(
             name: "sdk-ios",
-            targets: ["iZettleSDK", "iZettlePayments", "PPRiskMagnes"]),
+            targets: frameworks.map({ (name: String, checksum: String) in name }))
     ],
     dependencies: [],
-    targets: [
-        .binaryTarget(
-            name: "iZettleSDK", 
-            path: "./iZettleSDK/iZettleSDK.xcframework"),
-        .binaryTarget(
-            name: "iZettlePayments", 
-            path: "./iZettleSDK/iZettlePayments.xcframework"),
-        .binaryTarget(
-            name: "PPRiskMagnes",
-            path: "./iZettleSDK/PPRiskMagnes.xcframework")
-    ]
+    targets: frameworks.map({ (name: String, checksum: String) in Target.binaryTarget(
+        name: name,
+        url: "https://github.com/iZettle/sdk-ios/releases/download/\(version)/\(name).xcframework.zip",
+        checksum: checksum) })
 )
